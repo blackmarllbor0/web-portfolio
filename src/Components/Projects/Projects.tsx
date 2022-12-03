b42135ee95e1e1c822bb791c57e89dd7574e749e
Blackmarllbor0 <3100194@gmail.com>
2022-12-03 23:43:54 +0300

    компонет проектов

---
+++ src/Components/Projects/Projects.tsx
@@ -0,0 +1,11 @@
+import { Slider } from "../Slider/Slider";
+import "./Projects.scss";
+
+export const Projects = () => {
+  return (
+    <section className="projects">
+      <h1>Мои проекты</h1>
+      <Slider />
+    </section>
+  );
+};
