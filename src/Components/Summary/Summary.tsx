d5ccb29862183f6e8a1bed1de3688f09137ea431
Blackmarllbor0 <3100194@gmail.com>
2022-12-08 19:31:44 +0300

    исправил ошибку тексте

--- src/Components/Summary/Summary.tsx
+++ src/Components/Summary/Summary.tsx
@@ -4,7 +4,10 @@ import summary from "../../Assets/summary.pdf";
 export const Summary = () => {
   return (
     <section className="summary">
-      <p></p>
+      <h1>
+        Здесь вы можете скачать и ознокомиться с моим резюме. Так же снизу будут
+        указаны контакты для связи. До связи!
+      </h1>
       <div className="download_block">
         <a href="." download={summary} title={"Панов Даниил"}>
           Скачать резюме в .pdf
