46085a84d81ef00b625f4031bbf06c60e2a3baea
Blackmarllbor0 <3100194@gmail.com>
2022-12-08 21:36:16 +0300

    поправил тип для свг, чтобы можно было его использовать как компонент

--- src/Types/index.d.ts
+++ src/Types/index.d.ts
@@ -7,3 +7,11 @@ declare module "*.pdf" {
   const path: string;
   export default path;
 }
+
+declare module "*.svg" {
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGAttributes<SVGAElement>
+  >;
+  const path: string;
+  export default path;
+}
